3343774acb4e5c60fe3af2c75339478805099b4c
firdausillah <firdausillah123@gmail.com>
2024-01-22 21:40:49 +0700

    update db

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'https://pmibanyuwangi.or.id/',
+	'hostname' => 'pmibanyuwangi.or.id',
 	'username' => 'u767547842_seblang_wangi',
 	'password' => 'Pm1B4nyuw4ng1',
 	'database' => 'u767547842_seblang_wangi',
